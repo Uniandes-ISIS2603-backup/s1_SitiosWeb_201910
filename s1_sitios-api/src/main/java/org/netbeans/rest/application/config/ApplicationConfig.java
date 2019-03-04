4446ce58b72dbbea574d553c154feae7771b78b1
Santiago Ballesteros <s.ballesteros@uniandes.edu.co>
2019-03-04 07:30:41 -0500

    updt4

--- s1_sitios-api/src/main/java/org/netbeans/rest/application/config/ApplicationConfig.java
+++ s1_sitios-api/src/main/java/org/netbeans/rest/application/config/ApplicationConfig.java
@@ -29,6 +29,7 @@ public class ApplicationConfig extends Application {
      * If required, comment out calling this method in getClasses().
      */
     private void addRestResourceClasses(Set<Class<?>> resources) {
+        resources.add(TecnologiaSitioWebResourse.class);
         resources.add(co.edu.uniandes.csw.sitios.filters.CORSFilter.class);
         resources.add(co.edu.uniandes.csw.sitios.mappers.BusinessLogicExceptionMapper.class);
         resources.add(co.edu.uniandes.csw.sitios.mappers.ExceptionMapperA.class);
