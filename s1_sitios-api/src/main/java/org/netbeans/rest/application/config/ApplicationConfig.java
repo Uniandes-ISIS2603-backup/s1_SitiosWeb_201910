415e693bb7bbca68dc6f06e6da3cfd8f5944996c
Albert Molano <aa.molano@uniandes.edu.co>
2019-03-04 02:36:41 -0500

    por que sigue cambiando este a toda hora?

--- s1_sitios-api/src/main/java/org/netbeans/rest/application/config/ApplicationConfig.java
+++ s1_sitios-api/src/main/java/org/netbeans/rest/application/config/ApplicationConfig.java
@@ -29,7 +29,6 @@ public class ApplicationConfig extends Application {
      * If required, comment out calling this method in getClasses().
      */
     private void addRestResourceClasses(Set<Class<?>> resources) {
-        resources.add(co.edu.uniandes.csw.sitios.resources.TecnologiaResource.class);
         resources.add(co.edu.uniandes.csw.sitios.filters.CORSFilter.class);
         resources.add(co.edu.uniandes.csw.sitios.mappers.BusinessLogicExceptionMapper.class);
         resources.add(co.edu.uniandes.csw.sitios.mappers.ExceptionMapperA.class);
