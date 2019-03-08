5eecad0ec5fe7b517e7c744f4418bd7ad6776543
Allan <ar.corinaldi@uniandes.edu.co>
2019-03-08 16:12:10 -0500

    Aniado metodos de usuarioTickets logic y resource
    Importante!!!
    Aniado el nuevo nombre de la clase de usaurioTicket a usuarioTickets cuidado con el merge
    No aniado metodo delete para los tickets, no lo veo necesario.

--- s1_sitios-api/src/main/java/org/netbeans/rest/application/config/ApplicationConfig.java
+++ s1_sitios-api/src/main/java/org/netbeans/rest/application/config/ApplicationConfig.java
@@ -46,7 +46,7 @@ public class ApplicationConfig extends Application {
         resources.add(co.edu.uniandes.csw.sitios.resources.TecnologiaSitioWebResourse.class);
         resources.add(co.edu.uniandes.csw.sitios.resources.TicketResource.class);
         resources.add(co.edu.uniandes.csw.sitios.resources.UsuarioResource.class);
-        resources.add(co.edu.uniandes.csw.sitios.resources.UsuarioTicketResource.class);
+        resources.add(co.edu.uniandes.csw.sitios.resources.UsuarioTicketsResource.class);
     }
     
 }
